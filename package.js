6460fc6a8c9c4cebc6663f68f3023a2fabc0791c
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-07-29 13:47:11 +0530

    Using kadira:flow-router-ssr

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'kadira:react-layout',
   summary: 'Layout Manager for React with SSR Support',
-  version: '1.0.1',
+  version: '1.1.0',
   git: 'https://github.com/kadirahq/meteor-react-layout.git'
 });
 
