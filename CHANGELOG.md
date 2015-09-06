351014eab5c4d4fdc50e3ce56615342907264e99
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-09-06 06:32:45 +0530

    v1.2.1

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change Log
+
+### v1.2.1
+
+* Fix issue (FlowRouter#298)[https://github.com/kadirahq/flow-router/issues/298) where we try to use some non-standard way to render react components.
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'kadira:react-layout',
   summary: 'Layout Manager for React with SSR Support',
-  version: '1.2.0',
+  version: '1.2.1',
   git: 'https://github.com/kadirahq/meteor-react-layout.git'
 });
 
